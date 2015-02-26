6da3bdfd5a437f776e024926b6a916ebed7ca919
bob <bob@reshut1-48-17-dhcp.int.colorado.edu>
2015-02-26 16:38:42 -0700

    added rough menu and mouse scrollbar

--- SimpleJavaApplet.java
+++ SimpleJavaApplet.java
@@ -5,27 +5,57 @@ import java.awt.event.*;
 public class SimpleJavaApplet extends JApplet implements MouseListener{
 	GameBoard board;
 	String status;
+	JTextArea mouse_coordinates;
+	JScrollPane sp;
+	JFrame menu;
+	JButton start, instructions, settings;
 	
 	public void init(){
 		addMouseListener(this);
+		this.mouseCoordinatesArea();
+		this.createMenu();
 	}
+	
 	
 	public void paint(Graphics g){
 		board = new GameBoard();
 		board.drawBoard(g);
 		board.drawRedCheckers(g);
 		board.drawBlackCheckers(g);
-		g.clearRect(100, 400, 300, 150);
-		g.drawString(status, 100, 450);
 		//board.printCheckers();
 	}
 	
 	public void mouseEntered(MouseEvent me){}
 	public void mouseExited(MouseEvent me){}
 	public void mousePressed(MouseEvent me){
-		status = "Mouse button pressed at: x=" + me.getX() + " y=" + me.getY();
-		repaint();
+		status = "Mouse button pressed at: x=" + me.getX() + " y=" + me.getY() + "\n";
+		mouse_coordinates.append(status);
+		//repaint();
 	}
 	public void mouseReleased(MouseEvent me){}
-	public void mouseClicked(MouseEvent me){}			
+	public void mouseClicked(MouseEvent me){}
+		
+	public void mouseCoordinatesArea(){
+		// Output mousePressed to text area with scroll bars at bottom of window
+		setLayout(new BorderLayout());
+		mouse_coordinates = new JTextArea(5, 5);
+		sp = new JScrollPane(mouse_coordinates);
+		add(sp, BorderLayout.SOUTH);
+	}
+	
+	public void createMenu(){
+		// Creates the menu pop-up frame
+		menu = new JFrame("Menu");
+		menu.setLayout(new GridLayout(3,1));
+		menu.setLocation(200, 200);
+		start = new JButton("Start");
+		instructions = new JButton("Instructions");
+		settings = new JButton("Settings");
+		menu.add(start);
+		menu.add(instructions);
+		menu.add(settings);
+		//menu.pack();
+		menu.setSize(200, 200);
+		menu.setVisible(true);
+	}			
 }
