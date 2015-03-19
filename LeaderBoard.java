b8f2b59a23c885058c9c896b46f084f29c3b876e
bob <bob@engr2-16-52-dhcp.int.colorado.edu>
2015-03-19 14:16:29 -0600

    rough leaderboard completed

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -4,33 +4,78 @@ import java.awt.event.*;
 import java.io.*;
 import java.net.*;
 
-public class LeaderBoard{
+public class LeaderBoard implements ActionListener{
   String lfile = new String("leaderboard.txt");
 	JFrame frame;
   StringBuffer strBuff;
-	JTextArea txtArea;
-	JScrollPane l;
+	JTextArea lArea;
+  JLabel nameLabel;
+  JTextField nameInput;
+  JButton submitButton;
+  JPanel nameSubmitPane;
+	JScrollPane lBoardScroll;
 
 	public LeaderBoard(){
-		frame = new JFrame("LeaderBoard");
-		frame.setLayout(new BorderLayout());
-    txtArea = new JTextArea(100,100);
-		l = new JScrollPane(txtArea);
-    frame.add(l, BorderLayout.WEST);
-		frame.setSize(200, 200);
-		frame.setVisible(true);
-    writeFile();
-    readFile();
+    nameForm();
+    addListeners();
+    createLeaderBoard();
+    showLeaderBoard();		
 	}
 
+  /* Create the leaderboard frame and add components to it */
+  public void showLeaderBoard(){
+    frame = new JFrame("LeaderBoard");
+    frame.setLayout(new BorderLayout());
+    frame.add(nameSubmitPane, BorderLayout.SOUTH);
+    frame.add(lBoardScroll, BorderLayout.CENTER);
+    frame.pack();
+    frame.setVisible(true);
+    readFile();
+  }
+
+  /* Create the form for the submission of a name to the leaderboard */
+  public void nameForm(){
+    nameLabel = new JLabel("Submit name to leaderboard:");
+    nameInput = new JTextField(10);
+    submitButton = new JButton("Submit");
+    nameSubmitPane = new JPanel();
+    nameSubmitPane.add(nameLabel);
+    nameSubmitPane.add(nameInput);
+    nameSubmitPane.add(submitButton);
+  }
+
+  /* Create the text area for the leaderboard */
+  public void createLeaderBoard(){
+    lArea = new JTextArea(10,10);
+    lArea.setEditable(false);
+    lBoardScroll = new JScrollPane(lArea);
+  }
+
+
+  /* Detect the submission of a name */
+  public void actionPerformed(ActionEvent ae){
+    Object obj = ae.getSource();
+    if(obj == submitButton){
+        writeFile("\n" + nameInput.getText());
+        readFile();
+    }
+  }
+
+  /* Handle events in the frame */
+  public void addListeners(){
+    submitButton.addActionListener(this);
+  }
+
+  /* Read from the leaderboard.txt and put in the textarea */
   public void readFile(){
+    lArea.setText(null);
     String line;  
     try{
       InputStream in = LeaderBoard.class.getResourceAsStream(lfile);
       BufferedReader bf = new BufferedReader(new InputStreamReader(in));
-      txtArea.append("File Name : " + lfile + "\n");
+      //readArea.append("File Name : " + lfile + "\n");
       while((line = bf.readLine()) != null){
-        txtArea.append(line + "\n");
+        lArea.append(line + "\n");
       }
     }
     catch(IOException e){
@@ -38,11 +83,12 @@ public class LeaderBoard{
     }
  	}
 
-  public void writeFile(){
+  /* Write a string to leadership.txt */
+  public void writeFile(String text){
     try{
       File f = new File(lfile);
       BufferedWriter out = new BufferedWriter(new FileWriter(f, true));
-      out.write("\nBob");
+      out.write(text);
       out.close();
     }catch(IOException e){
       e.printStackTrace();
--- SimpleJavaApplet.java
+++ SimpleJavaApplet.java
@@ -6,35 +6,35 @@ import java.net.*;
 
 
 
-public class SimpleJavaApplet extends JApplet implements MouseListener{
+public class SimpleJavaApplet extends JApplet implements MouseListener, ActionListener{
 	GameBoard board;
 	LeaderBoard lboard;
 	String status;
 	JTextArea mouseCoordinates;
 	JScrollPane sp;
 	JFrame menu;
-	JButton start, instructions, settings;
+	JButton start, instructions, settings, leaderBoardB;
 	
 	public void init(){
-		lboard = new LeaderBoard();
-		/*board = new GameBoard();
+		//lboard = new LeaderBoard();
+		board = new GameBoard();
 		addMouseListener(this);
 		this.mouseCoordinatesArea();
-		this.createMenu();*/
+		this.createMenu();
 	}
 	
 	
 	public void paint(Graphics g){
-		//lboard = new LeaderBoard();
-		//board = new GameBoard();
-		/*board.drawBoard(g);
+		board.drawBoard(g);
 		board.drawRedCheckers(g);
 		board.drawBlackCheckers(g);
-		board.printCheckers();*/
+		board.printCheckers();
 	}
 	
 	public void mouseEntered(MouseEvent me){}
 	public void mouseExited(MouseEvent me){}
+
+	/* Outputs the mouse coordinates the the mouse texarea */
 	public void mousePressed(MouseEvent me){
 		status = "Mouse button pressed at: x=" + me.getX() + " y=" + me.getY() + "\n";
 		mouseCoordinates.append(status);
@@ -42,27 +42,41 @@ public class SimpleJavaApplet extends JApplet implements MouseListener{
 	}
 	public void mouseReleased(MouseEvent me){}
 	public void mouseClicked(MouseEvent me){}
-		
+	
+	/* Creates the text area that shows the mouse coordinates */	
 	public void mouseCoordinatesArea(){
-		// Output mousePressed to text area with scroll bars at bottom of window
 		setLayout(new BorderLayout());
 		mouseCoordinates = new JTextArea(5, 5);
 		sp = new JScrollPane(mouseCoordinates);
 		add(sp, BorderLayout.SOUTH);
 	}
+
+	/* Detect button presses */
+	public void actionPerformed(ActionEvent ae){
+		Object obj = ae.getSource();
+		if(obj == leaderBoardB){
+			lboard = new LeaderBoard();
+		}
+	}
 	
+	/* Creates the pop-up menu */
 	public void createMenu(){
-		// Creates the menu pop-up frame
 		menu = new JFrame("Menu");
 		menu.setLayout(new GridLayout(3,1));
 		menu.setLocation(200, 200);
+
+		leaderBoardB = new JButton("Leaderboard");
+		leaderBoardB.addActionListener(this);
+
 		start = new JButton("Start");
 		instructions = new JButton("Instructions");
 		settings = new JButton("Settings");
+
+		menu.add(leaderBoardB);
 		menu.add(start);
 		menu.add(instructions);
 		menu.add(settings);
-		//menu.pack();
+		
 		menu.setSize(200, 200);
 		menu.setVisible(true);
 	}
