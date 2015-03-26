aa7a196e08684b020ba900fb3a9d7ecd18d2bafd
hannahkeller <keller.hannah6@gmail.com>
2015-03-26 17:32:23 -0600

    added first attempt main menu and settings menu

---
+++ SettingsMenu.java
@@ -0,0 +1,97 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class SettingsMenu implements ActionListener {
+	JFrame setMenu;
+	MainMenu mainMenu;
+
+	private static SettingsMenu settingsMenu;
+
+	public static SettingsMenu getInstance(){
+		if(settingsMenu == null){
+			settingsMenu = new SettingsMenu();
+		}
+		settingsMenu.setMenu.setVisible(true);
+		return settingsMenu;
+	}
+
+	private SettingsMenu(){
+		setMenu = new JFrame("Settings");
+		setMenu.setLocation(200,200);
+
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		contentPane.setOpaque(true);
+		contentPane.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+		JLabel sound = new JLabel("Sound");
+		JPanel onoff = new JPanel();
+		onoff.setLayout(new FlowLayout());
+
+		JRadioButton on = new JRadioButton("On");
+		on.setSelected(true);
+		JRadioButton off = new JRadioButton("Off");
+
+		ButtonGroup group = new ButtonGroup();
+		group.add(on);
+		group.add(off);
+
+		onoff.add(on);
+		onoff.add(off);
+
+		JLabel visualStyles = new JLabel("Visual Styles");
+		String[] styleStrings = { "Red/Black", "White/Black", "Blue/Red", "Blue/Green" };
+		JComboBox styles = new JComboBox(styleStrings);
+		styles.setSelectedIndex(0);
+		styles.setMaximumSize(new Dimension(200,15));
+
+		JPanel menuButtons = new JPanel();
+		menuButtons.setLayout(new FlowLayout());
+
+		JButton save = new JButton("Save");
+		JButton mainMenu = new JButton("Main Menu");
+		mainMenu.setActionCommand("main");
+		menuButtons.add(save);
+		menuButtons.add(mainMenu);
+
+		on.addActionListener(this);
+		off.addActionListener(this);
+		styles.addActionListener(this);
+		save.addActionListener(this);
+		mainMenu.addActionListener(this);
+
+		sound.setAlignmentX(Component.CENTER_ALIGNMENT);
+		onoff.setAlignmentX(Component.CENTER_ALIGNMENT);
+		visualStyles.setAlignmentX(Component.CENTER_ALIGNMENT);
+		styles.setAlignmentX(Component.CENTER_ALIGNMENT);
+		save.setAlignmentX(Component.CENTER_ALIGNMENT);
+		mainMenu.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		contentPane.add(Box.createRigidArea(new Dimension(0,10)));
+		contentPane.add(sound);
+		contentPane.add(Box.createRigidArea(new Dimension(0,5)));
+		contentPane.add(onoff);
+		contentPane.add(visualStyles);
+		contentPane.add(Box.createRigidArea(new Dimension(0,5)));
+		contentPane.add(styles);
+		contentPane.add(Box.createRigidArea(new Dimension(0,5)));
+		contentPane.add(menuButtons);
+		contentPane.add(Box.createRigidArea(new Dimension(0,10)));
+
+		setMenu.add(contentPane);
+		setMenu.pack();
+		setMenu.setSize(250,250);
+		setMenu.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent ae){
+		if ("main".equals(ae.getActionCommand())){
+			mainMenu = MainMenu.getInstance();
+			setMenu.setVisible(false);
+		}
+	}	
+}
--- SimpleJavaApplet.java
+++ SimpleJavaApplet.java
@@ -6,9 +6,9 @@ import java.net.*;
 
 
 
-public class SimpleJavaApplet extends JApplet implements MouseListener, ActionListener{
+public class SimpleJavaApplet extends JApplet implements MouseListener{
 	GameBoard board;
-	LeaderBoard lboard;
+	MainMenu mainMenu;
 	String status;
 	JTextArea mouseCoordinates;
 	JScrollPane sp;
@@ -16,19 +16,18 @@ public class SimpleJavaApplet extends JApplet implements MouseListener, ActionLi
 	JButton start, instructions, settings, leaderBoardB;
 	
 	public void init(){
-		lboard = new LeaderBoard();
-		//board = new GameBoard();
-		//addMouseListener(this);
-		//this.mouseCoordinatesArea();
-		//this.createMenu();
+		board = new GameBoard();
+		mainMenu = MainMenu.getInstance();
+		addMouseListener(this);
+		this.mouseCoordinatesArea();
 	}
 	
 	
 	public void paint(Graphics g){
-		//board.drawBoard(g);
-		//board.drawRedCheckers(g);
-		//board.drawBlackCheckers(g);
-		//board.printCheckers();
+		board.drawBoard(g);
+		board.drawRedCheckers(g);
+		board.drawBlackCheckers(g);
+		board.printCheckers();
 	}
 	
 	public void mouseEntered(MouseEvent me){}
@@ -38,7 +37,7 @@ public class SimpleJavaApplet extends JApplet implements MouseListener, ActionLi
 	public void mousePressed(MouseEvent me){
 		status = "Mouse button pressed at: x=" + me.getX() + " y=" + me.getY() + "\n";
 		mouseCoordinates.append(status);
-		//repaint();
+		repaint();
 	}
 	public void mouseReleased(MouseEvent me){}
 	public void mouseClicked(MouseEvent me){}
@@ -49,35 +48,5 @@ public class SimpleJavaApplet extends JApplet implements MouseListener, ActionLi
 		mouseCoordinates = new JTextArea(5, 5);
 		sp = new JScrollPane(mouseCoordinates);
 		add(sp, BorderLayout.SOUTH);
-	}
-
-	/* Detect button presses */
-	public void actionPerformed(ActionEvent ae){
-		Object obj = ae.getSource();
-		if(obj == leaderBoardB){
-			lboard = new LeaderBoard();
-		}
-	}
-	
-	/* Creates the pop-up menu */
-	public void createMenu(){
-		menu = new JFrame("Menu");
-		menu.setLayout(new GridLayout(3,1));
-		menu.setLocation(200, 200);
-
-		leaderBoardB = new JButton("Leaderboard");
-		leaderBoardB.addActionListener(this);
-
-		start = new JButton("Start");
-		instructions = new JButton("Instructions");
-		settings = new JButton("Settings");
-
-		menu.add(leaderBoardB);
-		menu.add(start);
-		menu.add(instructions);
-		menu.add(settings);
-
-		menu.setSize(200, 200);
-		menu.setVisible(true);
 	}
 }
