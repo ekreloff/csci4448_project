1b6aa4d68bf793cef266fbbd87b3e12c3ff1fb6d
Ethan Kreloff <ekreloff@gmail.com>
2015-04-08 02:47:26 -0700

    Drawing board

--- GameScene.java
+++ GameScene.java
@@ -2,7 +2,25 @@ import java.awt.Graphics;
 
 public class GameScene extends Scene{
   public void draw(Graphics g){
-
+    drawBoard(g);
   }
+
+  public void drawBoard(Graphics g){
+		for(int i = 0; i <=7; i++){
+			this.drawRows(g, i, size);
+		}
+	}
+	
+	public void drawRows(Graphics g, int y, int size){
+		for(int i = 1;i <= 8; i++){
+			if((y+i)%2 == 1){
+				g.setColor(Color.lightGray);
+				g.fillRect(i*size, y*size, size, size);
+			}else{
+				g.setColor(Color.darkGray);
+				g.fillRect(i*size, y*size, size, size);
+			}
+		}
+	}
   ///*ARRAY OF CHEKERS, */
 }
